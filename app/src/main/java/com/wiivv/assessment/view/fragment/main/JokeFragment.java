c2261479b0adc6abbc7575a7f05bb0f24f35c50b
Daniel Marantz <dmarantz@dmarantz-mac01.local>
2018-05-12 19:51:18 -0700

    ui view fragment logic

--- app/src/main/java/com/wiivv/assessment/view/fragment/main/JokeFragment.java
+++ app/src/main/java/com/wiivv/assessment/view/fragment/main/JokeFragment.java
@@ -1,9 +1,16 @@
 package com.wiivv.assessment.view.fragment.main;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.SearchView;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.wiivv.assessment.presenter.main.JokePresenter;
 import com.wiivv.assessment.view.activity.main.MainActivity;
@@ -12,6 +19,7 @@ import com.wiivv.assessment.view.panel.JokePanel;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 
 /**
  * JokeFragment displays and searches for jokes.
@@ -28,6 +36,18 @@ public class JokeFragment extends BaseFragmentWithButterKnife implements JokePan
     @Inject
     JokePresenter presenter;
 
+    @BindView(R.id.joke_collapsing)
+    CollapsingToolbarLayout collapsingToolbarView;
+    @BindView(R.id.joke_search)
+    SearchView searchView;
+    @BindView(R.id.joke_fab)
+    FloatingActionButton fabView;
+    @BindView(R.id.joke_content)
+    TextView questionTextView;
+
+    @BindView(R.id.joke_progress_bar)
+    ProgressBar progressBar;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,11 +59,12 @@ public class JokeFragment extends BaseFragmentWithButterKnife implements JokePan
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initialView();
+        getDefaultJoke();
     }
 
     @Override
     public int getLayoutResId() {
-        return 0;
+        return R.layout.fragment_joke;
     }
 
     private void initialInjector() {
@@ -55,17 +76,73 @@ public class JokeFragment extends BaseFragmentWithButterKnife implements JokePan
     }
 
     private void initialView() {
-
+        initialToolbar();
+        initialSearchView();
+        initialListeners();
     }
 
+    private void initialToolbar() {
+        collapsingToolbarView.setTitle(getString(R.string.collapsing_toolbar_title));
+    }
+
+    private void initialSearchView() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                showProgressSpinner(true);
+                presenter.searchJoke(query);
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        searchView.setIconified(false);
+        searchView.requestFocus();
+    }
+
+    private void initialListeners() {
+        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+                }
+            }
+        });
+        fabView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                searchView.setIconified(false);
+                searchView.requestFocus();
+            }
+        });
+    }
+
+    private void getDefaultJoke() {
+        presenter.searchJoke(getString(R.string.default_search_term));
+    }
+
+    private void showProgressSpinner(boolean isVisible) {
+        progressBar.setVisibility(isVisible ? View.VISIBLE : View.GONE);
+    }
 
     @Override
     public void renderJoke(String joke) {
-
+        showProgressSpinner(false);
+        questionTextView.setText(joke);
     }
 
     @Override
     public void renderError() {
-
+        showProgressSpinner(false);
+        showSnackBar(getView(), getString(R.string.no_search_results));
     }
 }
