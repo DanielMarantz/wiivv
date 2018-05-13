86649145c31ddebcd765a41227840412f2e8a1ce
Daniel Marantz <dmarantz@dmarantz-mac01.local>
2018-05-12 19:59:30 -0700

    graphql and apollo query integration

---
+++ app/src/main/java/com/wiivv/assessment/helper/ApiHelper.java
@@ -0,0 +1,36 @@
+package com.wiivv.assessment.helper;
+
+import android.content.Context;
+
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.cache.normalized.CacheControl;
+import com.wiivv.assessment.api.ApiClient;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * ApiHelper is a com.wiivv.assessment.helper for the GraphQL queries.
+ * <p>
+ * NOTE: Helpers are injected into the presenters and should
+ * not touch the view layer.
+ */
+@Singleton
+public class ApiHelper {
+
+    private ApiClient mApiClient;
+
+    @Inject
+    public ApiHelper(Context context) {
+        mApiClient = new ApiClient(context);
+    }
+
+    public ApolloClient getApolloClient() {
+        return mApiClient.getApolloClient();
+    }
+
+    public ApolloCall<SearchJoke.Data> searchJoke(String keyword) {
+        return getApolloClient().query(new SearchJoke(keyword))
+                .cacheControl(CacheControl.CACHE_FIRST);
+    }
+}
--- app/src/main/java/com/wiivv/assessment/presenter/main/JokePresenter.java
+++ app/src/main/java/com/wiivv/assessment/presenter/main/JokePresenter.java
@@ -1,12 +1,18 @@
 package com.wiivv.assessment.presenter.main;
 
+import android.util.Log;
+
+import com.apollographql.apollo.rx2.Rx2Apollo;
 import com.wiivv.assessment.di.scope.PerSection;
+import com.wiivv.assessment.helper.ApiHelper;
 import com.wiivv.assessment.presenter.base.BasePresenter;
 import com.wiivv.assessment.view.panel.JokePanel;
 
 import javax.inject.Inject;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * JokePresenter is the presentation layer for the JokeFragment.
@@ -20,18 +26,48 @@ public class JokePresenter extends BasePresenter<JokePanel> {
 
     private static final String TAG = JokePresenter.class.getSimpleName();
 
-
+    private ApiHelper mApiHelper;
     private CompositeDisposable mDisposables;
 
     private JokePanel mPanel;
 
     @Inject
-    public JokePresenter() {
+    public JokePresenter(ApiHelper apiHelper) {
+        this.mApiHelper = apiHelper;
         this.mDisposables = new CompositeDisposable();
     }
 
     @Override
     public void setPanel(JokePanel panel) {
+        this.mPanel = panel;
+    }
 
+    public void searchJoke(final String keyword) {
+
+        mDisposables.add(Rx2Apollo.from(mApiHelper.searchJoke(keyword))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<Response<SearchJoke.Data>>() {
+
+                    @Override
+                    public void onSuccess(Response<SearchJoke.Data> dataResponse) {
+                        SearchJoke.Data data = dataResponse.data();
+                        SearchJoke.Joke jokeData = data.joke();
+
+                        if(jokeData != null) {
+                            Log.d(TAG, "searchJoke onSuccess");
+                            mPanel.renderJoke(jokeData.joke());
+                        } else {
+                            Log.e(TAG, "searchJoke No Result");
+                            mPanel.renderError();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, e.getMessage(), e);
+                        mPanel.renderError();
+                    }
+                }));
     }
 }
