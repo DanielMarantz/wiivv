e82946a302e937b728083c44ee83c5828ab8d458
Daniel Marantz <dmarantz@dmarantz-mac01.local>
2018-05-12 19:32:06 -0700

    presenter structure and base class

---
+++ app/src/main/java/com/wiivv/assessment/presenter/base/BasePresenter.java
@@ -0,0 +1,9 @@
+package com.wiivv.assessment.presenter.base;
+
+/**
+ * setPanel() will be called to pass view reference to com.wiivv.assessment.presenter
+ */
+public abstract class BasePresenter<P> {
+
+    public abstract void setPanel(P panel);
+}
